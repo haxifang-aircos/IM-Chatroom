74708d6e97b7880a4b49e4cfe0bf20b0f833fd4f
龚国玮 <gongguowei@haxifang.com>
2020-09-05 16:46:37 +0800

    feat: 服务端启动
    local: src/main/java/com/aircos/NettyClient.java
    
    detail: 非常精简的服务端启动 Demo, serverBootstrap 可选用的方法还有很多, 例如:
    	- childOption() 给每条连接指定自定义属性
    	- option() 给服务端 channel 设置一些属性
    	......

--- src/main/java/com/aircos/NettyClient.java
+++ src/main/java/com/aircos/NettyClient.java
@@ -1,4 +1,51 @@
 package com.aircos;
 
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * 服务端启动流程
+ */
 public class NettyClient {
+
+    private static final int BEGIN_PORT = 8000;
+
+    public static void main(String[] args) {
+
+        // 1.前置准备
+        // 监听端口线程组
+        NioEventLoopGroup bossGroup = new NioEventLoopGroup();
+
+        // 数据读写线程组
+        NioEventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        // 2.启动工作
+        final ServerBootstrap serverBootstrap = new ServerBootstrap();
+
+        serverBootstrap
+                .group(bossGroup, workerGroup)
+                .channel(NioServerSocketChannel.class)
+                .childHandler(new ChannelInitializer<NioSocketChannel>() {
+                    protected void initChannel(NioSocketChannel ch) {
+                        // 2.2 业务处理逻辑
+                    }
+                });
+
+        // 3.绑定端口
+        bind(serverBootstrap, BEGIN_PORT);
+    }
+
+    private static void bind(final ServerBootstrap serverBootstrap, final int port) {
+        serverBootstrap.bind(port).addListener(future -> {
+            if (future.isSuccess()){
+                System.out.println("端口[" + port + "]绑定成功!");
+            } else {
+                System.out.println("端口[" + port + "]绑定失败!");
+                bind(serverBootstrap, port + 1);
+            }
+        });
+    }
 }
